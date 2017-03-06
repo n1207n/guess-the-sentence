bb09a5a80f2f1eba346a66b413716cc1142b7934
Silin Na <n1207n@gmail.com>
2017-03-06 15:24:58 -0800

    docs: Code documentation for passportJS

--- backend/auth/strategies/jwt.js
+++ backend/auth/strategies/jwt.js
@@ -4,12 +4,20 @@ import {User} from '../../models';
 
 import {Strategy as JWTStrategy, ExtractJwt} from 'passport-jwt';
 
+// Parsing JWT value from Authorization header
+// JWT secret key for encrypting/decrypting from environment variable
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeader(),
   secretOrKey: process.env.JWT_SECRET,
 };
 
 export default new JWTStrategy(options, async (payload, done) => {
+  /**
+   * done callback parameters
+   * - JS error object or null
+   * - data or false boolean value
+   * - message string or null
+   */
   try {
     const user = await User.findById(payload.id).exec();
 
--- backend/auth/strategies/local.js
+++ backend/auth/strategies/local.js
@@ -4,9 +4,16 @@ import {User} from '../../models';
 
 import {Strategy as LocalStrategy} from 'passport-local';
 
+// email as username field
 const options = {usernameField: 'email'};
 
 export default new LocalStrategy(options, async (email, password, done) => {
+  /**
+   * done callback parameters
+   * - JS error object or null
+   * - data or false boolean value
+   * - message string or null
+   */
   try {
     const user = await User.findOne({email}).exec();
 
